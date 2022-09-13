b499e2cc2b7e2b29fc3a2e1764636b1b5a0b7992
lisaweilguni <lisaweilguni@gmx.at>
2022-09-13 17:42:45 +0200

    Add text input fields and generate button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState } from 'react';
+
+const divStyles = css`
+  color: purple;
+  text-align: center;
+  justify-content: center;
+  margin-top: 20em;
+`;
+
+function TextInputSection({ topText, setTopText, bottomText, setBottomText }) {
+  return (
+    <div css="divStyles">
+      {/*Input Section Top Text*/}
+      <label htmlFor="top-text">Top text:</label>
+
+      <input
+        id="top-text"
+        value={topText}
+        placeholder="Hello"
+        onChange={(event) => {
+          setTopText(event.currentTarget.value);
+          const topText = event.currentTarget.value;
+        }}
+      />
+      <br />
+      <br />
+      {/*Input Section Bottom Text*/}
+      <label htmlFor="bottom-text">Bottom text:</label>
+
+      <input
+        id="bottom-text"
+        value={bottomText}
+        placeholder="Hello"
+        onChange={(event) => {
+          setBottomText(event.currentTarget.value);
+          const bottomText = event.currentTarget.value;
+        }}
+      />
+    </div>
+  );
+}
 
 function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={divStyles}>
+      <h1>Create your custom meme</h1>
+      <TextInputSection
+        topText={topText}
+        bottomText={bottomText}
+        setTopText={setTopText}
+        setBottomText={setBottomText}
+      />
+      <br />
+      <br />
+      <button
+        onClick={(event) => {
+          setTopText(topText);
+          setBottomText(bottomText);
+        }}
+      >
+        Preview meme
+        <br />
+      </button>
     </div>
   );
 }
