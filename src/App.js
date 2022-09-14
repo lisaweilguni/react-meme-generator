ab0b8fe4167eddb555faf8da8da80a64035cd4e6
lisaweilguni <lisaweilguni@gmx.at>
2022-09-14 15:32:16 +0200

    Implement select bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import './App.css';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const divStyles = css`
   color: purple;
   text-align: center;
   justify-content: center;
-  margin-top: 20em;
+  margin-top: 40px;
 `;
 
-function TextInputSection({ topText, setTopText, bottomText, setBottomText }) {
+function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [userUrl, setUserUrl] = useState(
+    'https://api.memegen.link/images/success/hi/you.png',
+  );
+  const [userTemplate, setUserTemplate] = useState('');
+  const [data, setData] = useState([]);
+
+  // Fetch template
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.memegen.link/templates/');
+        const json = await response.json();
+        setData(json);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const templateList = data.map((template) => {
+    return (
+      <option value={template.id} key={template.id}>
+        {template.name}
+      </option>
+    );
+  });
+
   return (
-    <div css="divStyles">
+    <div css={divStyles}>
+      <h1>Create your custom meme</h1>
+
       {/*Input Section Top Text*/}
-      <label htmlFor="top-text">Top text:</label>
+      <label htmlFor="top-text">Top text</label>
 
       <input
         id="top-text"
@@ -28,7 +60,7 @@ function TextInputSection({ topText, setTopText, bottomText, setBottomText }) {
       <br />
       <br />
       {/*Input Section Bottom Text*/}
-      <label htmlFor="bottom-text">Bottom text:</label>
+      <label htmlFor="bottom-text">Bottom text</label>
 
       <input
         id="bottom-text"
@@ -39,34 +71,45 @@ function TextInputSection({ topText, setTopText, bottomText, setBottomText }) {
           const bottomText = event.currentTarget.value;
         }}
       />
-    </div>
-  );
-}
 
-function App() {
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
+      <br />
+      <br />
 
-  return (
-    <div css={divStyles}>
-      <h1>Create your custom meme</h1>
-      <TextInputSection
-        topText={topText}
-        bottomText={bottomText}
-        setTopText={setTopText}
-        setBottomText={setBottomText}
-      />
+      {/*Input Section Template*/}
+      <label htmlFor="user-template">Meme template</label>
+      <select
+        id="user-template"
+        value={userTemplate}
+        onChange={(event) => {
+          setUserTemplate(event.currentTarget.value);
+          const userTemplate = event.currentTarget.value;
+        }}
+      >
+        {templateList}
+      </select>
+
       <br />
       <br />
       <button
         onClick={(event) => {
-          setTopText(topText);
-          setBottomText(bottomText);
+          setUserUrl(
+            `https://api.memegen.link/images/${userTemplate}/${topText}/${bottomText}.png`,
+          );
         }}
       >
         Preview meme
-        <br />
       </button>
+      <br />
+      <br />
+      <button>Download</button>
+      <div>
+        <img
+          className="meme-image"
+          data-test-id="meme-image"
+          src={userUrl}
+          alt="Your meme"
+        />
+      </div>
     </div>
   );
 }
